de9d8b223fd8a7befc5e4dfbf7d52564fec41bd3
Evgeniy <antonm33@yandex.ru>
2019-05-27 18:18:22 +0200

    fix 1 step comments from mentor

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,12 @@
 module.exports = {
   presets: [
     [
-      "@babel/env",
+      '@babel/env',
       {
         targets: {
-          node: "current",
-          firefox: "60",
-          chrome: "67",
-          safari: "11.1"
-        }
-      }
-    ]
-  ]
+          node: 'current',
+        },
+      },
+    ],
+  ],
 };
