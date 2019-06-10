1783bdd4dfb063854333e327c49c3652bc436381
Evgeniy <antonm33@yandex.ru>
2019-06-10 16:46:14 +0200

    change name of func

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,11 +1,11 @@
 import playGame from '..';
 import generateRandom from '../utils';
 
-const progressionLength = 10;
+const progressionLength = 9;
 const task = 'What number is missing in the progression?';
-const createProgressionWithMissingMember = (start, diff, missingMemberIndex, length) => {
+const getQuestion = (start, diff, missingMemberIndex, length) => {
   let progression = '';
-  for (let i = 0; i < length; i += 1) {
+  for (let i = 0; i <= length; i += 1) {
     if (i === missingMemberIndex) {
       progression = `${progression} ..`;
     } else {
@@ -18,13 +18,8 @@ const createProgressionWithMissingMember = (start, diff, missingMemberIndex, len
 const createGameData = () => {
   const start = generateRandom(1, 10);
   const diff = generateRandom(1, 10);
-  const missingMemberIndex = generateRandom(1, progressionLength);
-  const question = createProgressionWithMissingMember(
-    start,
-    diff,
-    missingMemberIndex,
-    progressionLength,
-  );
+  const missingMemberIndex = generateRandom(0, progressionLength);
+  const question = getQuestion(start, diff, missingMemberIndex, progressionLength);
   const answer = String(start + diff * missingMemberIndex);
   return { question, answer };
 };
